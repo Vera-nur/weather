1b0927ea2f401051264456421a2ae3dbc05f76ae
Vera <135285088+Vera-nur@users.noreply.github.com>
2025-07-07 14:54:32 +0300

    create project

---
+++ weather/weather/weatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  weatherApp.swift
+//  weather
+//
+//  Created by Vera Nur on 7.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct weatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ weather/weatherTests/weatherTests.swift
@@ -0,0 +1,17 @@
+//
+//  weatherTests.swift
+//  weatherTests
+//
+//  Created by Vera Nur on 7.07.2025.
+//
+
+import Testing
+@testable import weather
+
+struct weatherTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
